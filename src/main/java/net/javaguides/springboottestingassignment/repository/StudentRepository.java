72b47f9fe13e891678569cdfb3beac1484ea3bd0
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-12 15:46:19 -0600

    Added findByEmail to StudentRepository, implemented unit tests for basic CRUD functionality

--- src/main/java/net/javaguides/springboottestingassignment/repository/StudentRepository.java
+++ src/main/java/net/javaguides/springboottestingassignment/repository/StudentRepository.java
@@ -4,5 +4,8 @@ package net.javaguides.springboottestingassignment.repository;
 import net.javaguides.springboottestingassignment.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
 }
