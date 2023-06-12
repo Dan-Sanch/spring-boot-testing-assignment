e773583c611a4ba0a0ecb491f413cf17e17ebd2b
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-12 13:35:58 -0600

    Initialized

---
+++ src/main/java/net/javaguides/springboottestingassignment/SpringBootTestingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboottestingassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/springboottestingassignment/SpringBootTestingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboottestingassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
